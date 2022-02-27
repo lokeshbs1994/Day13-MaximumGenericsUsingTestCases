711a566f2c3cd5660584e5f27f7e585e1d1d1e2c
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-27 17:45:48 +0530

    Added: program to find max in 3 integers

---
+++ src/main/java/com/bridgeLabz/maximum/Maximum.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz.maximum;
+
+public class Maximum {
+    public static Integer getmaximum(Integer value1, Integer value2, Integer value3) {
+        Integer max = value1;
+        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+            max = value2;
+        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+            max = value3;
+        System.out.println(max);
+        return max;
+    }
+}
---
+++ src/test/java/com/bridgeLabz/maximum/MaximumTest.java
@@ -0,0 +1,24 @@
+package com.bridgeLabz.maximum;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MaximumTest
+{
+    @Test
+    public void intMax1() {
+        int actualValue = Maximum.getmaximum(12,14,15);
+        Assert.assertEquals(15,actualValue);
+    }
+
+    @Test
+    public void intMax2() {
+        int actualValue = Maximum.getmaximum(1000,545,746);
+        Assert.assertEquals(1000,actualValue);
+    }
+}
