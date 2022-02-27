87ffcb0f81ba7f1969f37472ccfc6c4881bf4ad8
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-27 18:12:00 +0530

    Added:Refactored to Generic Method

--- src/main/java/com/bridgeLabz/maximum/Maximum.java
+++ src/main/java/com/bridgeLabz/maximum/Maximum.java
@@ -1,8 +1,8 @@
 package com.bridgeLabz.maximum;
 
 public class Maximum {
-    public static String getmaximum(String value1, String value2, String value3) {
-        String max = value1;
+    public static <E extends Comparable> E getmaximum(E value1, E value2, E value3) {
+        E max = value1;
         if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
             max = value2;
         else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
